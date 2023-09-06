6d7d31dcecc00dab0faa74a31f220613a833ef89
João Lucas <Jlucasoc@hotmail.com>
2023-09-05 21:48:15 -0300

    feat: ✨ creating temporary search page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,14 +11,15 @@ export default function Header() {
                 <Image src="/white-logo.svg" width={120} height={120} alt="BookMarks's logo" className="hidden dark:block" />
             </Link>
             <div className="flex w-full justify-between">
-                <div className="flex w-full relative">
+                <form action="/search" className="flex w-full relative">
                     <input
                         className="pr-4 pl-12 py-2 text-lg h-fit rounded-full border text-gray-600 border-gray-300 dark:border-none dark:bg-[#4B5B73] dark:text-white focus:outline-none focus:ring-2 focus:ring-primary-600 dark:focus:ring-white focus:border-transparent transition w-6/12 peer"
                         type="text"
                         id="search"
+                        name="q"
                     />
                     <FaMagnifyingGlass className="h-5 w-5 text-black/40 dark:text-white/40 absolute top-3 left-4 peer-focus:text-primary-600 dark:peer-focus:text-white" />
-                </div>
+                </form>
                 <ThemeSwitcher />
             </div>
         </div>
