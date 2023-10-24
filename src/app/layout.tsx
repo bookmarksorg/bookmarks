b6f6cfdc77831dbae173ea0dd17833c34ae796f5
João Lucas <jloc@ic.ufal.br>
2023-10-24 09:16:29 -0300

    feat: testing og images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "BookMarks, Junte-se a comunidade!",
     description: "Interaja com outros usuários e discuta seus livros favoritos!",
+    openGraph: {
+        type: "website",
+        locale: "pt_BR",
+        url: "https://bookmarks-bookmarks.vercel.app",
+        siteName: "BookMarks",
+        title: "BookMarks, Junte-se a comunidade!",
+        description: "Interaja com outros usuários e discuta seus livros favoritos!",
+        images: [
+            {
+                url: "https://bookmarks-bookmarks.vercel.app/login.png",
+                width: 1200,
+                height: 630,
+                alt: "BookMarks, Junte-se a comunidade!",
+            },
+        ],
+    },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
