71cb307cf9e153595e91d5782bd2255a8173cb28
João Lucas <jloc@ic.ufal.br>
2023-10-23 22:52:28 -0300

    fix: the definitive fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,6 @@
-import axios from "axios";
 import NextAuth from "next-auth/next";
-import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-                username: { label: "Username", type: "text" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials) {
-                if (!credentials) {
-                    throw new Error("No credentials");
-                }
-
-                if (!credentials.username || !credentials.password) {
-                    throw new Error("Invalid credentials");
-                }
-
-                const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/token/`, credentials);
-
-                if (!data) {
-                    throw new Error("Invalid credentials");
-                }
-
-                const { data: user } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
-                    headers: {
-                        Authorization: `Bearer ${data.access}`,
-                    },
-                });
-
-                if (!user) {
-                    throw new Error("Invalid credentials");
-                }
-
-                return {
-                    id: user.id,
-                    name: user.username,
-                    email: user.email,
-                    image: data.access,
-                    accessToken: data.access,
-                    refreshToken: data.refresh,
-                };
-            },
-        }),
-    ],
-    session: {
-        strategy: "jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    debug: true,
-};
+import { authOptions } from "../auth";
 
 const authHandler = NextAuth(authOptions);
 
